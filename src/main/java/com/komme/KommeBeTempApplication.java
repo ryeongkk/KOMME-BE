a8a2c852e370865ab0435515fcccc6f4bff4118a
pooreumjung <pooreumjung02@naver.com>
2026-07-16 17:06:01 +0900

    chore: 스프링 설정 추가

---
+++ src/main/java/com/komme/KommeBeTempApplication.java
@@ -0,0 +1,13 @@
+package com.komme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KommeBeTempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KommeBeTempApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/komme/KommeBeTempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KommeBeTempApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
